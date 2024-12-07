1f4deaccc2a5babc9e0da3e601d448114692644e
Jakazz1979 <robertfoss6479@gmail.com>
2024-12-07 05:04:25 -0600

    updates to project

--- CS50P/project/utils.py
+++ CS50P/project/utils.py
@@ -1,6 +1,7 @@
 import csv
 import os
 
+
 def save_to_file(data, filename):
     """
     Save the budget data to a CSV file.
@@ -9,17 +10,22 @@ def save_to_file(data, filename):
     - data: dictionary containing budget data
     - filename: string path to the file where data will be saved
     """
-    with open(filename, mode='w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=['category', 'amount', 'date'])
+    with open(filename, mode="w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=["category", "amount", "date"])
         writer.writeheader()
 
         for key, value in data.items():
             if isinstance(value, dict):
-                value['amount'] = f"{value['amount']:.2f}"  # Format amount to 2 decimal places
-                writer.writerow({'category': key, 'amount': value['amount'], 'date': value['date']})
+                value["amount"] = (
+                    f"{value['amount']:.2f}"  # Format amount to 2 decimal places
+                )
+                writer.writerow(
+                    {"category": key, "amount": value["amount"], "date": value["date"]}
+                )
             else:
                 value = f"{value:.2f}"  # Format amount to 2 decimal places for non-dict values
-                writer.writerow({'category': key, 'amount': value, 'date': ''})
+                writer.writerow({"category": key, "amount": value, "date": ""})
+
 
 def load_from_file(filename):
     """
@@ -37,13 +43,13 @@ def load_from_file(filename):
         return data
 
     try:
-        with open(filename, mode='r') as file:
+        with open(filename, mode="r") as file:
             reader = csv.DictReader(file)
             for row in reader:
-                key = row['category']
-                amount = float(row['amount'])
-                date = row['date']
-                data[key] = {'amount': amount, 'date': date}
+                key = row["category"]
+                amount = float(row["amount"])
+                date = row["date"]
+                data[key] = {"amount": amount, "date": date}
     except Exception as e:
         print(f"An error occurred while loading the file: {e}")
     return data
